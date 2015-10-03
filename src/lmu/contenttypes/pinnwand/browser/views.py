377e374cb2ab07fe244e1e6ac670e44b8cd84ac7
Manuel Reinhardt <reinhardt@syslab.com>
2015-10-03 08:07:16 +0100

    use abstract listing view from policy

--- src/lmu/contenttypes/pinnwand/browser/views.py
+++ src/lmu/contenttypes/pinnwand/browser/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from Products.CMFCore import permissions
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from plone import api
@@ -9,6 +8,7 @@ from zope.schema.interfaces import IContextAwareDefaultFactory
 
 from lmu.contenttypes.pinnwand.interfaces import IPinnwandFolder
 from lmu.policy.base.browser import _AbstractLMUBaseContentView
+from lmu.policy.base.browser import _AbstractLMUBaseListingView
 from lmu.policy.base.browser import _FrontPageIncludeMixin
 from lmu.policy.base.browser import _EntryViewMixin
 
@@ -17,47 +17,33 @@ def str2bool(v):
     return v is not None and v.lower() in ['true', '1']
 
 
-class _AbstractPinnwandListingView(_AbstractLMUBaseContentView):
-
-    def entries(self):
-        entries = []
-        if IPinnwandFolder.providedBy(self.context):
-            content_filter = {
-                'portal_type': 'Pinnwand Entry',
-                #'review_state': ['published', 'internal-published', 'internally_published'],
-            }
-            if self.request.get('author'):
-                content_filter['Creator'] = self.request.get('author')
-
-            pcatalog = self.context.portal_catalog
-
-            entries = pcatalog.searchResults(
-                content_filter,
-                sort_on='modified', sort_order='reverse',
-                b_size=int(self.request.get('b_size', '20')),
-                b_start=int(self.request.get('b_start', '0'))
-            )
-
-        return entries
-
-    def canAdd(self):
-        #current_user = api.user.get_current()
-        #import ipdb; ipdb.set_trace()
-        #return api.user.has_permission(permissions.AddPortalContent, user=current_user, obj=self.context)
-        return api.user.has_permission(permissions.AddPortalContent, obj=self.context)
-
-
-class ListingView(_AbstractPinnwandListingView):
+class ListingView(_AbstractLMUBaseListingView):
 
     template = ViewPageTemplateFile('templates/listing_view.pt')
+
+    DEFAULT_LIMIT = 20
+    portal_type = 'Pinnwand Entry'
+    container_interface = IPinnwandFolder
+    sort_on = 'modified'
+
+    def __init__(self, context, request):
+        super(ListingView, self).__init__(context, request)
+
+        if self.request.get('author'):
+            self.content_filter['Creator'] = self.request.get('author')
 
     def __call__(self):
         return self.template()
 
 
-class FrontPageIncludeView(_AbstractPinnwandListingView, _FrontPageIncludeMixin):
+class FrontPageIncludeView(_AbstractLMUBaseListingView, _FrontPageIncludeMixin):
 
     template = ViewPageTemplateFile('templates/frontpage_view.pt')
+
+    DEFAULT_LIMIT = 20
+    portal_type = 'Pinnwand Entry'
+    container_interface = IPinnwandFolder
+    sort_on = 'modified'
 
 
 class EntryView(_AbstractLMUBaseContentView, _EntryViewMixin):
